3dc9890d31a1389a47587f2138bce872a7998431
Christopher Jimerson <cmjimerson1s@gmail.com>
2024-04-09 17:16:43 +0200

    Adjusted prefabs, adding player, and created collision for nulltile types

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,51 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float distance;
-    private bool canMove;
+    private float distance = 1u;
 
 
     // Update is called once per frame
     void Update()
     {
 
-        if (canMove == true) {
             PlayerGridMovement();
-        }
     }
 
     private void Movement(Vector3 direction) {
-        
-        transform.Translate(direction * distance * Time.deltaTime);
+
+        transform.Translate(direction * distance);
+
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Tile")) {
-            canMove = true;
+            Debug.Log("True");
         } else {
-            canMove = false;
             Debug.Log("False");
         }
     }
 
     private void PlayerGridMovement() {
 
-        if (Input.GetKeyUp(KeyCode.W))
-            Movement(Vector3.forward);
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if (DetectTile(Vector3.forward)) {
+                Movement(Vector3.forward);
+            }
+        }
 
-        if (Input.GetKeyUp(KeyCode.A))
-            Movement(Vector3.left);
+        if (Input.GetKeyDown(KeyCode.A)) {
+            if (DetectTile(Vector3.left)) {
+                Movement(Vector3.left);
+            }
+        }
 
-        if (Input.GetKeyUp(KeyCode.S))
-            Movement(-Vector3.forward);
+        if (Input.GetKeyDown(KeyCode.S)) {
+            if (DetectTile(-Vector3.forward)) {
+                Movement(-Vector3.forward);
+            }
+        }
 
-        if (Input.GetKeyUp(KeyCode.D))
-            Movement(Vector3.right);
+        if (Input.GetKeyDown(KeyCode.D)) {
+            if (DetectTile(Vector3.right)) {
+                Movement(Vector3.right);
+            }
+        }
+
 
     }
+
+    private bool DetectTile(Vector3 direction) {
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, direction);
+
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.collider.CompareTag("Tile")) {
+                return true;
+            }
+        }
+        return false;
+       
+    }
+
+
 
 }
