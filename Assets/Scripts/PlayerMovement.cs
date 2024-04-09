4179f5dc3a8644d4dd1a4e27ef2076e42a13179a
Christopher Jimerson <cmjimerson1s@gmail.com>
2024-04-09 15:25:57 +0200

    Create prefabs and polish gameobject and mesh location

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float distance;
+    private bool canMove;
 
 
     // Update is called once per frame
     void Update()
     {
+
+        if (canMove == true) {
+            PlayerGridMovement();
+        }
+    }
+
+    private void Movement(Vector3 direction) {
+        
+        transform.Translate(direction * distance * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Tile")) {
+            canMove = true;
+        } else {
+            canMove = false;
+            Debug.Log("False");
+        }
+    }
+
+    private void PlayerGridMovement() {
+
         if (Input.GetKeyUp(KeyCode.W))
             Movement(Vector3.forward);
 
@@ -25,16 +48,4 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    private void Movement(Vector3 direction) {
-        
-        transform.Translate(direction * distance * Time.deltaTime);
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Tile")) {
-            Debug.Log(other.gameObject.name);
-        } else {
-            Debug.Log("Error");
-        }
-    }
 }
