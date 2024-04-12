951b9bf1aa9dafc29550fad73b221c341197e88e
Christopher Jimerson <cmjimerson1s@gmail.com>
2024-04-12 15:17:58 +0200

    Adjusted main menu text/buttons and added coloured panel to canvas

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource menuMusic;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     
+        menuMusic.Play();
     }
 
     // Update is called once per frame
