3ad9709c7b92d9ffed336356fb2ce7e6c4e31185
Christopher Jimerson <cmjimerson1s@gmail.com>
2024-08-20 14:30:33 +0200

    Began Save/Load game profile scene

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -4,19 +4,79 @@ using UnityEngine;
 using UnityEngine.Windows;
 using System.IO;
 using TMPro;
+using static UnityEditor.FilePathAttribute;
+using File = System.IO.File;
+using Newtonsoft.Json;
 
-public class LevelSelect : MonoBehaviour
-{
+public class LevelSelect : MonoBehaviour {
+    private IDataService DataService = new JsonDataService();
     private string filePath;
-    public TMP_InputField profileOne;
+    private bool EncryptionEnabled;
+    public TMP_InputField profileOneInput;
+    public GameObject SelectButtonOne;
+    public TextMeshProUGUI profileOneName;
 
     void Start() {
+        DisableTMPInputs();
+        CollectProfileData();
         filePath = Application.persistentDataPath;
         string[] files = System.IO.Directory.GetFiles(filePath);
         Debug.Log(files[0]);
         foreach (string file in files) {
             string fileName = Path.GetFileName(file);
-            Debug.Log("File:" +  fileName);
+            Debug.Log("File:" + fileName);
         }
+    }
+
+    public void CollectProfileData() {
+        string profile = "/user-profiles.json";
+        filePath = Application.persistentDataPath + profile;
+        if (File.Exists(filePath)) {
+            List<string> profileNames = DataService.LoadData<List<string>>(profile, EncryptionEnabled);
+            DisplayProfileData(profileNames);
+        } else {
+            Debug.Log("No file found");
+            List<string> profileNames = new List<string>();
+            DisplayProfileData(profileNames);
+        }
+    }
+
+    public void DisplayProfileData(List<string> profileNames) {
+        if (profileNames.Count < 1) {
+            Debug.Log("No names found");
+        } else {
+            foreach (string profileName in profileNames) {
+                Debug.Log(profileName);
+            }
+        }
+    }
+
+    public void NewGame(List<string> profileList) {
+        string profileName = profileOneInput.text;
+        profileList.Add(profileName);
+        SaveProfile(profileList);
+
+    }
+
+    public void SaveProfile(List<string> profileNames) {
+        if (DataService.SaveData("/user-profiles.json", profileNames, EncryptionEnabled)) {
+            Debug.Log("Saved Successfully");
+        } else {
+            Debug.LogError("Could not save file! Show something on the UI about it!");
+        }
+
+    }
+
+    public void DisableTMPInputs() {
+        profileOneInput.gameObject.SetActive(false);
+    }
+
+    public void DisableSelectButton() {
+        SelectButtonOne.gameObject.SetActive(false);
+        profileOneInput.gameObject.SetActive(true);
+        profileOneInput.Select();
+        profileOneInput.ActivateInputField();
+        profileOneName.SetText("Enter Name");
+
     }
 }
