7d5042a15a3115a2ac285f4b11b048dc548ba2dc
Christopher Jimerson <cmjimerson1s@gmail.com>
2024-08-21 14:43:48 +0200

    Began design of level select screen

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.UI;
+
 
 public class LevelSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject levelUI;
+    public int levelID;
+
+    public void Start() {
+        LockedLevel();
+    }
+    public void UnlockedLevel() {
+        Image lockImage = levelUI.transform.Find("Lock")?.GetComponent<Image>();
+        TextMeshProUGUI levelText = transform.GetComponentInChildren<TextMeshProUGUI>();
+        levelText.text = $"#{levelID.ToString()}";
+        lockImage.enabled = false;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LockedLevel() {
+        Image backgroundImg = levelUI.transform.Find("BackGround")?.GetComponent<Image>();
+        backgroundImg.color = new Color(0.2196078f, 0.2196078f, 0.2196078f, 1f);
+        Button selectButton = levelUI.transform.Find("SelectButton")?.GetComponent<Button>();
+        selectButton.gameObject.SetActive(false); 
+        Image numImg = levelUI.transform.Find("NumImage")?.GetComponent<Image>();
+        numImg.color = new Color(0.2196078f, 0.2196078f, 0.2196078f, 1f);
+        Image lockImage = levelUI.transform.Find("Lock")?.GetComponent<Image>();
+        lockImage.color = Color.white;
+
+
     }
+
 }
---
+++ Assets/Scripts/LevelSelectController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectUI : MonoBehaviour
+{
+    public GameObject levelOne;
+    public GameObject levelTwo;
+    public GameObject levelThree;
+    public GameObject levelFour;
+    public GameObject levelFive;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
